7088b2031e1c73390ac2249caf39d808de4ee28d
Андрей Исаков <deflorator1980@yandex.ru>
2018-07-25 23:16:09 +0300

    pocketsphinx

--- README.txt
+++ README.txt
@@ -1,3 +1,6 @@
 https://realpython.com/python-speech-recognition/
-
 sudo pip3 install SpeechRecognition
+
+https://github.com/cmusphinx/pocketsphinx-python
+sudo apt-get install -y python3 python3-dev python3-pip build-essential swig git libpulse-dev
+sudo pip install pocketsphinx
--- speach_to_text.py
+++ speach_to_text.py
@@ -1,8 +1,9 @@
 import speech_recognition as sr
 r = sr.Recognizer()
-# harvard = sr.AudioFile('hz.wav')
-harvard = sr.AudioFile('english.wav')
+harvard = sr.AudioFile('hz.wav')
+# harvard = sr.AudioFile('english.wav')
 with harvard as source:
     audio = r.record(source)
-print(r.recognize_google(audio))
+# print(r.recognize_google(audio))
+print(r.recognize_sphinx(audio))
 
