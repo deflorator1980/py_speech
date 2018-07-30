4c87e971a219d986876fc15421dd860fc25a95aa
Андрей Исаков <deflorator1980@yandex.ru>
2018-07-30 23:43:03 +0300

    upload wav

--- README.txt
+++ README.txt
@@ -5,7 +5,10 @@ https://github.com/cmusphinx/pocketsphinx-python
 sudo apt-get install -y python3 python3-dev python3-pip build-essential swig git libpulse-dev
 sudo pip3 install pocketsphinx
 
+or
+
 sudo docker-compose pull
 sudo docker-compose up
-On localhost:5000 text from file hz.wav
+
+On localhost:5000 upload *.wav file and get speech to text
 
--- speech_to_flask.py
+++ speech_to_flask.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 import speech_recognition as sr
 
 app = Flask(__name__)
 
 def speech():
     r = sr.Recognizer()
-    harvard = sr.AudioFile('hz.wav')
-    # harvard = sr.AudioFile('english.wav')
+    harvard = sr.AudioFile('file.wav')
     with harvard as source:
         audio = r.record(source)
     return r.recognize_google(audio)
 
 @app.route('/')
-def index():
-    text = speech()
-    return text
+def upload():
+   return render_template('upload.html')
 
-app.run(host='0.0.0.0', debug=True)   # reboot the page by changing
+@app.route('/upload', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+       f = request.files['file']
+       f.save(secure_filename("file.wav"))
+       text = speech()
+       return text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True) 
